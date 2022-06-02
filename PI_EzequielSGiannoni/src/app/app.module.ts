49a61c24e45650b4f2dfa4427c5942ec48e8706d
Ezequiel <egiannoni@frba.utn.edu.ar>
2022-06-02 18:01:20 -0300

    Agregando IDPhoto

--- PI_EzequielSGiannoni/src/app/app.module.ts
+++ PI_EzequielSGiannoni/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
-import { HttpClientModule } from '@angular/common/http' ;
+import { HttpClientModule } from '@angular/common/http';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component' ;
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent
+    SocialComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
