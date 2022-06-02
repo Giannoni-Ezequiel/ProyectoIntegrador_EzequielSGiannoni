9c21fd977e161e6d19cbd4cc5c47ead5556eeda7
Ezequiel <egiannoni@frba.utn.edu.ar>
2022-06-02 02:02:06 -0300

    Assets + bootstrap + angular

--- PI_EzequielSGiannoni/src/app/app.module.ts
+++ PI_EzequielSGiannoni/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { HttpClientModule } from '@angular/common/http' ;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
